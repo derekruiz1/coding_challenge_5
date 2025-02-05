e48437f6c2ba674ebe3ae43e453b27d2ef1c9bd1
Derek Ruize <derekruiz@usf.edu>
2025-02-04 22:19:43 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,11 @@
+//Task 1 - Object Properties 
+let customer = {
+    name: "John Doe",
+    age: 35,
+    email: "Johndoe@email.com"
+}; //Object Information
+
+console.log(`Name: ${customer.name}`); //Displaying Name
+console.log(`Age: ${customer.age}`); //Displaying Age
+console.log(`Email: ${customer.email}`); //Displaying Email
+

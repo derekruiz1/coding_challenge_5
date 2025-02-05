c74929c7351ee400973ed8258756ced0b93be2be
Derek Ruize <derekruiz@usf.edu>
2025-02-04 22:49:27 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -9,3 +9,17 @@ console.log(`Name: ${customer.name}`); //Displaying Name
 console.log(`Age: ${customer.age}`); //Displaying Age
 console.log(`Email: ${customer.email}`); //Displaying Email
 
+//Task 2 - Object Methods
+const order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing",
+    displayOrder: function displayOrder() {
+        console.log(`Order ID: ${this.orderId}`)
+        console.log(`Total Amount: ${this.totalAmount}`)
+        console.log(`Order Status: ${this.status}`)
+    }
+    
+}; //Object Information, Displays Order ID, Total Amount, Order Status
+
+order.displayOrder();
